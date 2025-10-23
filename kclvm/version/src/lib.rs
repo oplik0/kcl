b17d7dc4d3e291141e0f32b9f060a71b311b211c
Jakub Bliźniuk <opliko.reg@protonmail.com>
2025-10-23 22:13:22 +0200

    just do env!

--- kclvm/version/src/lib.rs
+++ kclvm/version/src/lib.rs
@@ -1,9 +1,6 @@
 //! Copyright The KCL Authors. All rights reserved.
 
-pub const VERSION: &str = match option_env!("CARGO_PKG_VERSION") {
-  Some(v) => v,
-  None => include_str!("./../../../VERSION"),
-};
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const CHECK_SUM: &str = "c020ab3eb4b9179219d6837a57f5d323";
 pub const GIT_SHA: &str = env!("VERGEN_GIT_SHA");
 pub const HOST_TRIPLE: &str = env!("VERGEN_RUSTC_HOST_TRIPLE");
